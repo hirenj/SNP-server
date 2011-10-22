791f40df9973c5c4e110a2c8d80d6a1db9f7d506
Hiren Joshi <hirenj@gmail.com>
2011-10-22 00:57:11 +0000

    Fix up the s3 mounting script to actually work

--- bin/setup_s3fs.sh
+++ bin/setup_s3fs.sh
@@ -6,17 +6,16 @@ S3MNTDIR=$BASE/s31001proteomes
 FASTADIR=$S3MNTDIR/translated
 SNPDIR=$S3MNTDIR/snps
 GATORDIR=$S3MNTDIR/gator-snps
-REFDATA=$S3MNTDIR/TAIR-data
+REFDATA=$S3MNTDIR/tair-data
 
 LOCAL_OUT=out
 LOCAL_FASTAS=fastas
 LOCAL_SNPS=snps
+LOCAL_REFDATA=ref-data
 
+
+sudo umount $S3MNTDIR
 s3fs -o passwd_file=$BASE/passwd-s3fs 1001proteomes $S3MNTDIR
-if $?
-then
-    exit $?
-fi
 
 if [ ! -d $S3MNTDIR ]; then
     mkdir $S3MNTDIR
@@ -26,6 +25,16 @@ if [ ! -d $REFDATA ]; then
     mkdir $REFDATA
 fi
 
+for tairver in "tair8" "tair9" "tair10"
+do
+    if [ ! -d $REFDATA/$tairver ]; then
+        mkdir $REFDATA/$tairver
+    fi
+    if [ ! -d "$REFDATA/$tairver/$tairver""_pseudochromosomes" ]
+    then
+        mkdir "$REFDATA/$tairver/$tairver""_pseudochromosomes"
+    fi
+done
 if [ ! -d $FASTADIR ]; then
     mkdir $FASTADIR
 fi
@@ -48,4 +57,8 @@ fi
 
 if [ ! -d $LOCAL_SNPS ]; then
     ln -s $SNPDIR $LOCAL_SNPS
+fi
+
+if [ ! -d $LOCAL_REFDATA ]; then
+    ln -s $REFDATA $LOCAL_REFDATA
 fi
