40ca4254ab746c19cca6e48d3d566d7258653163
Hiren Joshi <hirenj@gmail.com>
2011-10-27 16:17:39 -0700

    Fix up path names for generated FAS files

--- bin/generate_indexes.sh
+++ bin/generate_indexes.sh
@@ -12,13 +12,13 @@ echo "<html><head><title>Fasta files</title><link rel=\"stylesheet\" type=\"text
 for acc in `ls -1 -d $fastadir/* | sort`; do
     accname=${acc##*/}
     echo "<div><h2>$accname</h2><ul>" >> $fastadir/index.html
-    echo "<li><a href=\"$acc/protein_chr1.fas\">Chromosome 1</a></li>" >> $fastadir/index.html
-    echo "<li><a href=\"$acc/protein_chr2.fas\">Chromosome 2</a></li>" >> $fastadir/index.html
-    echo "<li><a href=\"$acc/protein_chr3.fas\">Chromosome 3</a></li>" >> $fastadir/index.html
-    echo "<li><a href=\"$acc/protein_chr4.fas\">Chromosome 4</a></li>" >> $fastadir/index.html
-    echo "<li><a href=\"$acc/protein_chr5.fas\">Chromosome 5</a></li>" >> $fastadir/index.html
-    echo "<li><a href=\"$acc/protein_chrm.fas\">Chromosome M</a></li>" >> $fastadir/index.html
-    echo "<li><a href=\"$acc/protein_chrm.fas\">Chromosome C</a></li>" >> $fastadir/index.html
+    echo "<li><a href=\"$accname/protein_chr1.fas\">Chromosome 1</a></li>" >> $fastadir/index.html
+    echo "<li><a href=\"$accname/protein_chr2.fas\">Chromosome 2</a></li>" >> $fastadir/index.html
+    echo "<li><a href=\"$accname/protein_chr3.fas\">Chromosome 3</a></li>" >> $fastadir/index.html
+    echo "<li><a href=\"$accname/protein_chr4.fas\">Chromosome 4</a></li>" >> $fastadir/index.html
+    echo "<li><a href=\"$accname/protein_chr5.fas\">Chromosome 5</a></li>" >> $fastadir/index.html
+    echo "<li><a href=\"$accname/protein_chrm.fas\">Chromosome M</a></li>" >> $fastadir/index.html
+    echo "<li><a href=\"$accname/protein_chrm.fas\">Chromosome C</a></li>" >> $fastadir/index.html
     echo "</ul></div>" >> $fastadir/index.html
 done
 
