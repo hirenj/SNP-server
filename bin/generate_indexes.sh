03f2ec8bed338394bb96ffaa4b08e8ea327f737f
Hiren Joshi <hirenj@gmail.com>
2011-11-04 14:55:24 -0700

    Change the HTML for the page to be more verbose

--- bin/generate_indexes.sh
+++ bin/generate_indexes.sh
@@ -27,14 +27,14 @@ fi
 
 for acc in "${ECOTYPES[@]}"; do
     echo $acc
-    echo "<div><h2>$acc</h2><ul>" >> $tmpindex
-    echo "<li><a href=\"$acc/protein_chr1.fas\">Chromosome 1</a></li>" >> $tmpindex
-    echo "<li><a href=\"$acc/protein_chr2.fas\">Chromosome 2</a></li>" >> $tmpindex
-    echo "<li><a href=\"$acc/protein_chr3.fas\">Chromosome 3</a></li>" >> $tmpindex
-    echo "<li><a href=\"$acc/protein_chr4.fas\">Chromosome 4</a></li>" >> $tmpindex
-    echo "<li><a href=\"$acc/protein_chr5.fas\">Chromosome 5</a></li>" >> $tmpindex
-    echo "<li><a href=\"$acc/protein_chrm.fas\">Chromosome M</a></li>" >> $tmpindex
-    echo "<li><a href=\"$acc/protein_chrc.fas\">Chromosome C</a></li>" >> $tmpindex
+    echo "<div><h2>$acc protein pseudochromosomes</h2><ul>" >> $tmpindex
+    echo "<li><a href=\"$acc/protein_chr1.fas\">pep_chr1</a></li>" >> $tmpindex
+    echo "<li><a href=\"$acc/protein_chr2.fas\">pep_chr2</a></li>" >> $tmpindex
+    echo "<li><a href=\"$acc/protein_chr3.fas\">pep_chr3</a></li>" >> $tmpindex
+    echo "<li><a href=\"$acc/protein_chr4.fas\">pep_chr4</a></li>" >> $tmpindex
+    echo "<li><a href=\"$acc/protein_chr5.fas\">pep_chr5</a></li>" >> $tmpindex
+    echo "<li><a href=\"$acc/protein_chrm.fas\">pep_chrm</a></li>" >> $tmpindex
+    echo "<li><a href=\"$acc/protein_chrc.fas\">pep_chrc</a></li>" >> $tmpindex
     echo "</ul></div>" >> $tmpindex
 done
 
